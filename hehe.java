5b6007180f5dbd462a69d9ab1a68c335e853c696
Apoorv Pandey <102942046+pandeymonium@users.noreply.github.com>
2023-07-20 10:43:16 +0530

    Add files via upload

---
+++ hehe.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+
+class hehe
+{   public static void main(String args[])
+
+	{	String name;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Who are you?");
+		name = sc.nextLine();
+		
+		System.out.println("I am called "+name);
+	}
+
+}		
